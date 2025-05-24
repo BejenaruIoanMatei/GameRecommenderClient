9181916ee7bf2fbcff87ac5a4f6d4da4267ab8a2
BejenaruIoanMatei <bejematei@gmail.com>
2025-05-24 20:27:02 +0300

    2. HomeController

---
+++ GamesRecommenderClient/src/main/java/codes/GameRec/game_recommender_client_spring/GameRecommenderClientSpringApplication.java
@@ -0,0 +1,13 @@
+package codes.GameRec.game_recommender_client_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameRecommenderClientSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameRecommenderClientSpringApplication.class, args);
+    }
+
+}
